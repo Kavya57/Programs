d6d6bd1fb89ad167ca79d7ddab4ebd80e169d3eb
Kavya57 <59258767+Kavya57@users.noreply.github.com>
2020-01-03 12:21:37 +0530

    Add files via upload

---
+++ Users.java
@@ -0,0 +1,31 @@
+package com.del.first.entity;
+public class Users
+{
+  String username;
+  String password;
+public Users(String username, String password)
+{
+	super();
+	this.username = username;
+	this.password = password;
+}
+public Users() {}
+public String getUsername() 
+{
+	return username;
+}
+public void setUsername(String username) 
+{
+	this.username = username;
+}
+public String getPassword()
+{
+	return password;
+}
+public void setPassword(String password)
+{
+	this.password = password;
+}
+  
+  
+}
