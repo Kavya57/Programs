52072cb76b5af020808392ab7143a3746e1009fd
Kavya57 <59258767+Kavya57@users.noreply.github.com>
2020-01-03 17:54:39 +0530

    Add files via upload

--- AuthenticateServlet.java
+++ AuthenticateServlet.java
@@ -1,6 +1,8 @@
 package com.del.first.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,12 +19,18 @@ public void doPost(HttpServletRequest request,HttpServletResponse response) thro
 	  response.setContentType("text/html");
 	  String username=request.getParameter("uid");
 	  String password = request.getParameter("pwd");
+	  Cookie c= new Cookie("username",username);
+	  response.addCookie(c);
 	  Users u= new Users(username,password);
 	  RequestDispatcher rd= null;
 	  PrintWriter out=response.getWriter();
 	  out.println("<h1><center>");
 	  if(new UsersDAO().validateUser(u))
-		  out.println("Welcome to the website");
+	  {
+		  //out.println("Welcome to the website");
+		  rd=request.getRequestDispatcher("services.html");
+		  rd.forward(request, response);
+	  }
 	  else
 	  {
 		  out.println("Invalid Username/ Password");
